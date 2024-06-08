f50abb128daaf19c1e695a0c131bd699e4689353
eha49 <habron.rasheed7@gmail.com>
2024-06-08 10:23:02 +0530

    minor tweaking

--- src/Panel/Panel.jsx
+++ src/Panel/Panel.jsx
@@ -1,10 +1,12 @@
 function Panel() {
   return (
-    <aside className="flex flex-col items-center basis-60 grow py-8 bg-gradient-to-b from-blue-500 via-blue-600 to-blue-700 rounded-3xl">
-      <h1 className="text-base text-slate-200">Your Result</h1>
-      <div className="w-32 h-32 rounded-full flex flex-col justify-center items-center my-4 bg-gradient-to-b from-blue-800  via-blue-700 to-transparent shadow">
-        <h2>76</h2>
-        <p>of 100</p>
+    <aside className="flex flex-col items-center basis-60 grow py-6 bg-gradient-to-b from-blue-500 v to-blue-600 rounded-3xl">
+      <h1 className="text-base text-slate-200 opacity-85">
+        Your Result
+      </h1>
+      <div className="w-32 h-32 rounded-full flex flex-col justify-center items-center gap-2 my-6 bg-gradient-to-b from-blue-800  via-blue-600 to-transparent shadow">
+        <h2 className="font-bold text-5xl text-slate-50">76</h2>
+        <p className="text-xs text-slate-200 opacity-60">of 100</p>
       </div>
       <h3>Great</h3>
       <p>
